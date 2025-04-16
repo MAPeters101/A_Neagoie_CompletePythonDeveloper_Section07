00ac4b2601803c46b3e56709512cce125743ff68
Mark Peters <mapeters101@earthlink.net>
2025-04-16 12:31:06 -0400

    Work on lesson142.

---
+++ lesson142.py
@@ -0,0 +1,43 @@
+
+def multiply_by2(li):
+    new_list = []
+    for item in li:
+        new_list.append(item*2)
+    return new_list
+
+print(multiply_by2([1,2,3]))
+print()
+
+
+def multiply_by2(li):
+    new_list = []
+    for item in li:
+        new_list.append(item*2)
+    return print(new_list)
+
+multiply_by2([1,2,3])
+print()
+
+
+new_list = []
+def multiply_by2(li):
+    for item in li:
+        new_list.append(item*2)
+    return print(new_list)
+
+multiply_by2([1,2,3])
+print()
+
+
+# new_list = []
+# def multiply_by2(li):
+#     for item in li:
+#         new_list.append(item*2)
+#     return print(new_list)
+#
+# new_list = ''
+# multiply_by2([1,2,3])
+# print()
+
+
+
