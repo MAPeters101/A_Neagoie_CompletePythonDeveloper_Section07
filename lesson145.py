e9c58368de41cb55cec85f9ae4c703592f5f5830
Mark Peters <mapeters101@earthlink.net>
2025-04-16 18:28:02 -0400

    Work on lesson145.

---
+++ lesson145.py
@@ -0,0 +1,38 @@
+my_list = [1,2,3]
+your_list = [10,20,30]
+
+print(list(zip(my_list, your_list)))
+print(my_list)
+print(your_list)
+print()
+
+
+my_list = [1,2,3]
+your_list = (10,20,30)
+
+print(list(zip(my_list, your_list)))
+print(my_list)
+print(your_list)
+print()
+
+
+my_list = (1,2,3)
+your_list = (10,20,30)
+
+print(list(zip(my_list, your_list)))
+print(my_list)
+print(your_list)
+print()
+
+
+my_list = (1,2,3)
+your_list = (10,20,30)
+their_list = (5,4,3)
+
+print(list(zip(my_list, your_list, their_list)))
+print(my_list)
+print(your_list)
+print(their_list)
+print()
+
+
